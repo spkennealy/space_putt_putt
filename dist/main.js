2d8cbddc42a34b5ca40ee9f4bdca24163f4f6214
Sean Kennealy <spkennealy@gmail.com>
2019-06-26 10:28:58 -0700

    Add hole 9

--- dist/main.js
+++ dist/main.js
@@ -238,7 +238,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n\n\n/* harmony default export
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n// TODO: Edit coordinate\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n    holePos: [100, 70],\n    walls: [\n        [0, 140, 600, 20],\n        [200, 340, 600, 20],\n        [150, 40, 20, 60]\n    ],\n    triangles: [[]],\n    trianglesColor: \"rgb(128, 128, 128, 0.6)\",\n    wallColor: \"rgb(128, 128, 128, 0.6)\",\n    golfBallColor: \"#0000FF\",\n    matPos: [720, 375, 50, 100],\n    ballPos: [745, 425],\n    par: 3\n});\n\n//# sourceURL=webpack:///./src/holes/hole_9.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n\n\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\n    holePos: [700, 250],\n    walls: [\n        [600, 15, 30, 50],\n        [600, 85, 30, 50],\n        [600, 155, 30, 50],\n        [600, 225, 30, 50],\n        [600, 295, 30, 50],\n        [600, 365, 30, 50],\n        [600, 435, 30, 50]\n    ],\n    triangles: [[]],\n    trianglesColor: \"rgb(128, 128, 128, 0.6)\",\n    wallColor: \"rgb(128, 128, 128, 0.6)\",\n    golfBallColor: \"#0000FF\",\n    matPos: [40, 190, 50, 100],\n    ballPos: [70, 250],\n    par: 3\n});\n\n//# sourceURL=webpack:///./src/holes/hole_9.js?");
 
 /***/ }),
 
